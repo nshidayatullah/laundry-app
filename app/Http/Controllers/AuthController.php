4ac972ee9a6a2326c75651dead2c3385cd6a4f11
nshidayatullah <127877651+nshidayatullah@users.noreply.github.com>
2025-08-27 13:51:05 +0800

    login to dashboard

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,16 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function login()
     {
+
         return view('auth.login');
     }
 
-    public function authenticate(Request $request){
-        dd($request->all());
+    public function authenticate(Request $request)
+    {
+
+        $request->validate([
+            'email' => 'required|email:rfc,dns',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+        if (!$user) {
+            return back()->withErrors([
+                'email' => 'Email not found'
+            ]);
+        }
+
+        if (!Auth::attempt([
+            'email' => $request->email,
+            'password' => $request->password
+        ])) {
+            return back()->withErrors([
+                'password' => 'Incorrect password',
+            ]);
+        }
+
+
+        return redirect()->route('dashboard');
     }
 }
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
+    <!-- Theme Check and Update -->
+    <script>
+        const html = document.querySelector('html');
+        const isLightOrAuto = localStorage.getItem('hs_theme') === 'light' || (localStorage.getItem('hs_theme') ===
+            'auto' && !window.matchMedia('(prefers-color-scheme: dark)').matches);
+        const isDarkOrAuto = localStorage.getItem('hs_theme') === 'dark' || (localStorage.getItem('hs_theme') === 'auto' &&
+            window.matchMedia('(prefers-color-scheme: dark)').matches);
+
+        if (isLightOrAuto && html.classList.contains('dark')) html.classList.remove('dark');
+        else if (isDarkOrAuto && html.classList.contains('light')) html.classList.remove('light');
+        else if (isDarkOrAuto && !html.classList.contains('dark')) html.classList.add('dark');
+        else if (isLightOrAuto && !html.classList.contains('light')) html.classList.add('light');
+    </script>
+    @yield('css')
+</head>
+</head>
+
+<body class="bg-gray-50 dark:bg-neutral-900">
+
+    {{-- @include('layouts.header')
+
+    @include('layouts.sidebar') --}}
+
+    @yield('content')
+
+    @livewireScripts
+    @stack('scripts')
+</body>
+
+</html>
